ea1184f0fccb9b80e86915cf38ffad7f4023ab1f
sebamarambio96 <s.marambiosandoval@gmail.com>
2024-01-29 08:21:55 -0300

    agregue una ruta por default, para mostrar un ejemplo

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // Example request
+    return redirect('/api/get-currency-values/dolar/?start_date=2023-01-01&end_date=2024-01-29');
 });
